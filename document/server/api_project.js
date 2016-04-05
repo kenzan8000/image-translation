a54f60e81e33ce59ec2cc7895ec1cfeba456a1f4
kenzan8000 <kenzan8000@gmail.com>
2016-04-05 10:12:56 -0700

    update server document

--- document/server/api_project.js
+++ document/server/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-04-04T17:55:17.925Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-04-05T17:12:45.027Z",    "url": "http://apidocjs.com",    "version": "0.13.1"  }});
